97e468a9ac81589b966ad4b9f1aefa9a53333f9e
nazzz1324 <ovovkopo@gmail.com>
2024-10-22 19:55:26 +0500

    s

--- homework5.py
+++ homework5.py
@@ -1,13 +1,14 @@
 s = 0
-
+n = 1
 while True:
-    x = input("Введите число:")
+    x = input(f"Введите число {n}-ое:")
 
     if x == "stop" or x == "end":
         break
 
     if x.replace('.', '', 1).replace('-', '', 1).isdigit():
         s += float(x)
+        n += 1
     else:
         print("Ошибка: введите число")
 
